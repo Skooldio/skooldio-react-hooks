7cae50eb52fb87f6c7804f51cf0a1429c9dd28cb
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-03-20 15:49:12 +0700

    ✨ More Hooks - useRef

--- src/CounterPage.js
+++ src/CounterPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Wrapper, CounterText, Button, Label, Input } from "./Components";
 
 const getInitialCounter = () =>
@@ -10,6 +10,7 @@ export const CounterPage = () => {
   const [initialCounter, setInitialCounter] = useState(0);
   const [counter, setCounter] = useState(0);
   const [loading, setLoading] = useState(false);
+  const inputEl = useRef(null);
 
   useEffect(() => {
     setLoading(true);
@@ -18,6 +19,12 @@ export const CounterPage = () => {
       setInitialCounter(initialCounter);
     });
   }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      inputEl.current.focus();
+    }
+  }, [loading]);
 
   useEffect(() => {
     let id;
@@ -50,6 +57,7 @@ export const CounterPage = () => {
       <Label>
         <span>Initial Counter</span>
         <Input
+          ref={inputEl}
           value={initialCounter}
           onChange={(e) => setInitialCounter(e.target.value)}
         />
