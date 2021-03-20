c8102dc3af656f1bc63c334d99c577966e93f9b9
Panjamapong Sermsawatsri <panjmp@gmail.com>
2021-03-20 14:59:35 +0700

    ✨ Effect Hook

--- src/CounterPage.js
+++ src/CounterPage.js
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Wrapper, CounterText, Button } from "./Components";
 
 export const CounterPage = () => {
   const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounter((prevCounter) =>
+        prevCounter > 0 ? prevCounter - 1 : prevCounter
+      );
+    }, 1000);
+  }, []);
+
   return (
     <Wrapper>
       <CounterText>{counter}</CounterText>
